60daa26d1d06bbd3f783d40e4f7b2cbc5a4b68f4
HassanBahati <mukisabahati@gmail.com>
2021-05-13 17:13:40 +0300

    created db

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
+#imports 
 from flask import Flask , render_template, url_for
 from flask_sqlalchemy import SQLAlchemy 
 from datetime import datetime
 
+#configurations 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL']='sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+# model for the content 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200),nullable=False)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
+#function returning a string everytime a new entry is created
     def __repr__(self):
         return '<Task %r>' %self.id
 
